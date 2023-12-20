2310886112b5c9e29174f8808ee42a32916903f4
leonard <leogurning@gmail.com>
2023-12-20 22:40:48 +0700

    fix html, style. script. remove profile page

--- js/script.js
+++ js/script.js
@@ -8,11 +8,20 @@ setName(visitorName);
 function validateForm() {
     const name = document.forms["message-form"]["full-name"].value;
     const birthDate = document.forms["message-form"]["birth-date"].value;
-    const gender = document.forms["message-form"]["gender"].value;
+    let gender = document.forms["message-form"]["gender"].value;
+    if (gender === 'male') {
+        gender = "Laki-Laki";
+    } else if (gender === 'female') {
+        gender = "Perempuan";
+    } else {
+        gender = "Tidak Terdefinisi";
+    }
+    
     const messages = document.forms["message-form"]["messages"].value;
 
     if (name == "" || birthDate == "" || gender == "" || messages == "") {
         alert("Tidak boleh ada yang kosong");
+        setSenderUI("", "", "", "");    
         return false;
     }
         
@@ -26,3 +35,31 @@ function setSenderUI(pName, pBirthDate, pGender, pMessages) {
     document.getElementById("sender-gender").innerHTML = pGender;
     document.getElementById("sender-messages").innerHTML = pMessages;
 }
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+    showDivs((slideIndex += n));    
+}
+
+function showDivs(n) {
+    var i;
+    var imgList = document.getElementsByClassName("img-slideshow");
+    
+    if (n > imgList.length) {
+        slideIndex = 1;
+    } else if (n < 1) {
+        slideIndex = imgList.length;
+    }
+
+    for (i = 0; i < imgList.length; i++) {
+        imgList[i].style.display = "none";
+    }
+
+    imgList[slideIndex - 1].style.display = "block";
+}
+
+setInterval(() => {
+    plusDivs(1);
+}, 3000)
